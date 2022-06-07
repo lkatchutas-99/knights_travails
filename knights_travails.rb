f5bc369d9acb4508ff6b4872dcfbd2186c6f5e38
lkatchutas-99 <96667853+lkatchutas-99@users.noreply.github.com>
2022-06-06 23:46:14 -0400

    Shorten methods and add comments
    Methods that had redundant operations, were not adhering to DRY,
    or contained unused variables were shortened, divided, and/or removed
    entirely
    
    Added informative comments

--- knights_travails.rb
+++ knights_travails.rb
@@ -19,6 +19,7 @@ end
 class Knight
   attr_accessor :root
   def initialize(initial_position = [0, 0])
+    # coordinates
     @moves = { 
       up_right: [1, 2], 
       right_up: [2, 1],
@@ -36,50 +37,41 @@ class Knight
 
   # validate all moves in current square
   def create_square(position, square)
+    create_children_squares(square, position)
+  end
 
-    # give coordindates to possible moves
-    square.up_right = validate(position, @moves[:up_right])
-    square.right_up = validate(position, @moves[:right_up])
-    square.right_down = validate(position, @moves[:right_down])
-    square.down_right = validate(position, @moves[:down_right])
-    square.down_left = validate(position, @moves[:down_left])
-    square.left_down = validate(position, @moves[:left_down])
-    square.left_up = validate(position, @moves[:left_up])
-    square.up_left = validate(position, @moves[:up_left])
-    
-    # initialize parent to each move
-    square.up_right.parent = square unless square.up_right.nil?
-    square.right_up.parent = square unless square.right_up.nil?
-    square.right_down.parent = square unless square.right_down.nil?
-    square.down_right.parent = square unless square.down_right.nil?
-    square.down_left.parent = square unless square.down_left.nil?
-    square.left_down.parent = square unless square.left_down.nil?
-    square.left_up.parent = square unless square.left_up.nil?
-    square.up_left.parent = square unless square.up_left.nil?
-
+  # set current square's children
+  def create_children_squares(square, position)
+    square.up_right = validate(square, position, @moves[:up_right])
+    square.right_up = validate(square, position, @moves[:right_up])
+    square.right_down = validate(square, position, @moves[:right_down])
+    square.down_right = validate(square, position, @moves[:down_right])
+    square.down_left = validate(square, position, @moves[:down_left])
+    square.left_down = validate(square, position, @moves[:left_down])
+    square.left_up = validate(square, position, @moves[:left_up])
+    square.up_left = validate(square, position, @moves[:up_left])
     square
   end
 
-
-  def validate(position, moves)
+  # check if square meets validation
+  def validate(parent, position, moves)
     position = position.map.with_index { |coordinate, idx| coordinate + moves[idx] }
     return unless position.all? { |coordinate| coordinate >= 0 && coordinate <= 7 }
     
-    Square.new(positions)
+    square = Square.new(position)
+    square.parent = parent
+    square
   end
 
+  # move knight to new location
   def move_knight(new_location)
-    queue = []
     current_square = @root
-    locations_visited = []
-    current_square = bfs(current_square, queue)
-    path = print_path(current_square)
-    @root = current_square
-    @root.parent = nil
-    path
+    print_path(bfs(current_square, new_location))
   end
 
-  def bfs(current_square, queue)
+  # level order search
+  def bfs(current_square, new_location)
+    queue = []
     until current_square.data == new_location
       queue.push(current_square.up_right) unless current_square.up_right.nil?
       queue.push(current_square.right_up) unless current_square.right_up.nil?
@@ -92,8 +84,10 @@ class Knight
       next_square = queue.shift
       current_square = create_square(next_square.data, next_square)
     end
+    current_square
   end
 
+  # print path between origin and destination
   def print_path(current_square)
     path = []
     until current_square.data == @root.data
